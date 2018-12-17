eab14dac075940073ca09aebadcc92dd818b70e3
Sam Serrels <s.serrels@napier.ac.uk>
2018-12-17 13:39:30 +0000

    scripts

--- bash.sh
+++ bash.sh
@@ -1,9 +1,28 @@
-# CPUSEQ
-./../../BUILD/cputest/Release/main.exe -a0 --bmin 0 --bmax 10000 --bs 100 -r 2 -s 0 | tee CPUSEQ.csv
-# CPUOMP                                                                    
-./../../BUILD/cputest/Release/main.exe -a0 --bmin 0 --bmax 10000 --bs 100 -r 2 -s 0 | tee CPUOMP.csv                                                        
-./../../BUILD/cputest/Release/main.exe -a0 --bmin 0 --bmax 10000 --bs 100 -r 2 -s 1 | tee CPUOMP_s1.csv
-./../../BUILD/cputest/Release/main.exe -a0 --bmin 0 --bmax 10000 --bs 100 -r 2 -s 5 | tee CPUOMP_s5.csv                                                   
-./../../BUILD/cputest/Release/main.exe -a0 --bmin 0 --bmax 10000 --bs 100 -r 2 -s 10 | tee CPUOMP_s10.csv                                                         
-./../../BUILD/cputest/Release/main.exe -a0 --bmin 0 --bmax 10000 --bs 100 -r 2 -s 50 | tee CPUOMP_s50.csv                                                        
-./../../BUILD/cputest/Release/main.exe -a0 --bmin 0 --bmax 10000 --bs 100 -r 2 -s 100| tee CPUOMP_s100.csv
+S=$(date +"%H_%M")
+EXE=../../BUILD/cputest/Release/main.exe
+#EXE=main
+RUNS=2
+TICKS=1000
+./$EXE -a0 -f $TICKS --bmin 0 --bmax 1000 --bs 100 -r $RUNS -s 0  | tee ${S}_CPUSEQ_${TICKS}_${RUNS}_0.csv                                                       
+./$EXE -a1 -f $TICKS --bmin 0 --bmax 1000 --bs 100 -r $RUNS -s 0  | tee ${S}_CPUOMP_${TICKS}_${RUNS}_0.csv                                                        
+./$EXE -a1 -f $TICKS --bmin 0 --bmax 1000 --bs 100 -r $RUNS -s 1  | tee ${S}_CPUOMP_${TICKS}_${RUNS}_1.csv
+./$EXE -a1 -f $TICKS --bmin 0 --bmax 1000 --bs 100 -r $RUNS -s 5  | tee ${S}_CPUOMP_${TICKS}_${RUNS}_5.csv                                                   
+./$EXE -a1 -f $TICKS --bmin 0 --bmax 1000 --bs 100 -r $RUNS -s 10 | tee ${S}_CPUOMP_${TICKS}_${RUNS}_10.csv                                                         
+./$EXE -a1 -f $TICKS --bmin 0 --bmax 1000 --bs 100 -r $RUNS -s 50 | tee ${S}_CPUOMP_${TICKS}_${RUNS}_50.csv                                                        
+./$EXE -a1 -f $TICKS --bmin 0 --bmax 1000 --bs 100 -r $RUNS -s 100| tee ${S}_CPUOMP_${TICKS}_${RUNS}_100.csv
+
+./$EXE -a0 -f $TICKS --bmin 1000 --bmax 4000 --bs 100 -r $RUNS -s 0  | tee ${S}_CPUSEQ_${TICKS}_${RUNS}_0.csv                                                       
+./$EXE -a1 -f $TICKS --bmin 1000 --bmax 4000 --bs 100 -r $RUNS -s 0  | tee ${S}_CPUOMP_${TICKS}_${RUNS}_0.csv                                                        
+./$EXE -a1 -f $TICKS --bmin 1000 --bmax 4000 --bs 100 -r $RUNS -s 1  | tee ${S}_CPUOMP_${TICKS}_${RUNS}_1.csv
+./$EXE -a1 -f $TICKS --bmin 1000 --bmax 4000 --bs 100 -r $RUNS -s 5  | tee ${S}_CPUOMP_${TICKS}_${RUNS}_5.csv                                                   
+./$EXE -a1 -f $TICKS --bmin 1000 --bmax 4000 --bs 100 -r $RUNS -s 10 | tee ${S}_CPUOMP_${TICKS}_${RUNS}_10.csv                                                         
+./$EXE -a1 -f $TICKS --bmin 1000 --bmax 4000 --bs 100 -r $RUNS -s 50 | tee ${S}_CPUOMP_${TICKS}_${RUNS}_50.csv                                                        
+./$EXE -a1 -f $TICKS --bmin 1000 --bmax 4000 --bs 100 -r $RUNS -s 100| tee ${S}_CPUOMP_${TICKS}_${RUNS}_100.csv
+
+./$EXE -a0 -f $TICKS --bmin 4000 --bmax 10000 --bs 100 -r $RUNS -s 0  | tee ${S}_CPUSEQ_${TICKS}_${RUNS}_0.csv                                                             
+./$EXE -a1 -f $TICKS --bmin 4000 --bmax 10000 --bs 100 -r $RUNS -s 0  | tee ${S}_CPUOMP_${TICKS}_${RUNS}_0.csv                                                        
+./$EXE -a1 -f $TICKS --bmin 4000 --bmax 10000 --bs 100 -r $RUNS -s 1  | tee ${S}_CPUOMP_${TICKS}_${RUNS}_1.csv
+./$EXE -a1 -f $TICKS --bmin 4000 --bmax 10000 --bs 100 -r $RUNS -s 5  | tee ${S}_CPUOMP_${TICKS}_${RUNS}_5.csv                                                   
+./$EXE -a1 -f $TICKS --bmin 4000 --bmax 10000 --bs 100 -r $RUNS -s 10 | tee ${S}_CPUOMP_${TICKS}_${RUNS}_10.csv                                                         
+./$EXE -a1 -f $TICKS --bmin 4000 --bmax 10000 --bs 100 -r $RUNS -s 50 | tee ${S}_CPUOMP_${TICKS}_${RUNS}_50.csv                                                        
+./$EXE -a1 -f $TICKS --bmin 4000 --bmax 10000 --bs 100 -r $RUNS -s 100| tee ${S}_CPUOMP_${TICKS}_${RUNS}_100.csv
